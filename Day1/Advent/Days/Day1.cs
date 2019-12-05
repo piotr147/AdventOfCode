7b41814754dc41570376889f0ede2f5369a9c0bd
Piotr Koskiewicz (PIT) <PIT@kmd.dk>
2019-12-05 18:37:23 +0100

    Day 1

---
+++ Day1/Advent/Days/Day1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advent.Days
+{
+    public static class Day1
+    {
+        public static int GetResult(List<int> data)
+        {
+            int result = 0;
+            //var data = Helper.GetNumbersFromFile("Input1.txt");
+
+            foreach (var number in data)
+            {
+                result += number / 3 - 2;
+            }
+
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+}
---
+++ Day1/Advent/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Advent.Days;
+
+namespace Advent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool success = false;
+            int n = 0;
+
+            Console.Write("Enter day number: ");
+
+            while (!success)
+            {
+                try
+                {
+                    success = true;
+                    string day = Console.ReadLine();
+                    n = Int32.Parse(day);
+                }
+                catch (Exception e)
+                {
+                    success = false;
+                    Console.Write("\nEnter valid day number: ");
+                }
+            }
+
+            Console.WriteLine();
+
+            switch (n)
+            {
+                case 1:
+                    var data = GetListFromFile(@"..\..\..\Input\Input1.txt");
+                    Day1.GetResult(data);
+                    break;
+            }
+
+        }
+
+        public static List<int> GetListFromFile(string fileName)
+        {
+            var list = new List<int>();
+            int fuel = 0;
+
+            using (StreamReader sr = new StreamReader("Input.txt"))
+            {
+                String line = sr.ReadLine();
+
+                while (line != null)
+                {
+                    list.Add(Int32.Parse(line));
+                    line = sr.ReadLine();
+                }
+            }
+
+            return list;
+        }
+    }
+}
